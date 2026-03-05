d1ceb95c911f18893c74ded41164bf3efb319363
Prof Mata <profmata@Joses-MacBook-Pro.local>
2026-03-05 16:28:12 -0500

    Updated content for Week 6 on 3.5.2026

--- Week 6/recursion_1.py
+++ Week 6/recursion_1.py
@@ -8,7 +8,7 @@ def iterative(number):
     for i in range(1, number):
         print(f"Counting number {i}")
 
-#iterative(100)
+iterative(1000)
 
 #Notice in the recursive function below how if you uncomment line 17 and comment out line 19, the numbers are counted from 10 to 1. However, if you comment out line 17 and uncomment line 19, the numbers are counted from 1 to 10. This happens because the print statement in line 19 only happens AFTER the recursive function has reached its conclusion (after it has fully executed), so the first time this happens is when the base condition is achieved when number = 0. After number == 0, the call recursive(1 - 1) has concluded and the following statement is "Counting number 1." After this, the call recursive(2 - 1) is concluded and the following statement is "Counting number 2". And so on and so forth. This works like a "stack" of "frames" where the most recent function to be called needs to be concluded before the one before it can be concluded. However, there is a limit to how many "frames" or un-concluded functions can be in the stack at a time, so be mindful of that. 
 def recursive(number):
@@ -18,4 +18,4 @@ def recursive(number):
     recursive(number - 1)
     print(f"Counting number {number}")
 
-recursive(1000)
+#recursive(1000)
